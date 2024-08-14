ba0910939625456ead5cf49f462a78342a81dcf0
Emerson Freitas <mersonsi@gmail.com>
2024-08-13 22:36:09 -0300

    Extract arrange fase to beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,12 +1,15 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from '../../settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings'
 describe('WordleBoard', () => {
   let wordOfTheDay = 'hello'
-  
-  it('a victory message is displayed when the user makes a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  let wrapper: ReturnType<typeof mount>
 
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  })
+  
+  test('a victory message is displayed when the user makes a guess that matches the word of the day', async () => {
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue('hello')
     await guessInput.trigger('keydown.enter')
@@ -14,13 +17,16 @@ describe('WordleBoard', () => {
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 
-  it('a defeat message is displayed when the user makes a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
-
+  test('a defeat message is displayed when the user makes a guess that matches the word of the day', async () => {
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue('wrong')
     await guessInput.trigger('keydown.enter')
 
-    expect(wrapper.text()).toContain('Better luck next time!')
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  })
+
+  test('no end-of-game message is displayed when the user makes a guess that matches the word of the day', async () => {  
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
 })
--- src/settings.ts
+++ src/settings.ts
@@ -1,1 +1,2 @@
 export const VICTORY_MESSAGE = 'Congratulations! You guessed the word of the day!'
+export const DEFEAT_MESSAGE = 'Better luck next time!'
